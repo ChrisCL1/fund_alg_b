9ee1b7bcf6bfcfb66b0b13f160584c9318ba28ad
ChrisCL1 <2109048@upy.edu.mx>
2022-01-30 12:11:32 -0500

    Estructura General C

--- unit1/generalStrucProgramC.c
+++ unit1/generalStrucProgramC.c
@@ -8,9 +8,12 @@ int unEntero = 3;
 float unDecimal = 3.1416;
 char* name = "Christopher";
 char unCaracter = 'M';
-////////////////////////////////////////
+/////////////////////////////////
 
+
+/// La ejecucion del programa ///
 int main(){
-    
+        printf("Hello Data B %s\n", name);
     return 0;
 }
+/////////////////////////////////
