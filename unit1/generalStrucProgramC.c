10627dbde3259cc004285f4e171e67d4c876b23f
ChrisCL1 <2109048@upy.edu.mx>
2022-01-30 11:46:05 -0500

    created some variables

---
+++ unit1/generalStrucProgramC.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//* Global variables *;
+//Crate a variable: dataType nameVariable = anyData;
+//dataTypes in C/C++/C#/PHP 
+int unEntero = 3;
+float unDecimal = 3.1416;
+char* name = "Christopher";
+char unCaracter = 'M';
+////////////////////////////////////////
+
+int main(){
+    
+    return 0;
+}
