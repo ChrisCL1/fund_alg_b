112515756b65f166d4b88ec0e768ec8828f39fb6
ChrisCL1 <2109048@upy.edu.mx>
2022-02-22 15:39:13 -0600

    evenOdd

--- unit1/evenOdd.c
+++ unit1/evenOdd.c
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int main(int argc, char *argv[]){
-    int numberToTest = *argv[1];
+int main(int argc, char** argv){
+
+    if (argc != 2){
+        printf("Error program: U need to insert a number, e.g: /%s argv[1]\n", argv[0]);
+    return 0;
+    }
+
+    bool isANumber = (argv[1][0] >= 48 && argv[1][0] <= 57);
+
+    if (!isANumber){
+        printf("Error program: U need to insert just a number not a letter, e.g: %s 8\n", argv[0]);
+    return 0;
+    }
+
+    int numberToTest = atoi(argv[1]);
     int result = numberToTest % 2;
     bool hasModule = (result > 0);
 
